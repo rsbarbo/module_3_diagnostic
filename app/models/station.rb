4a749463dd71188f1e65ae00d76eb55cd39d3a55
Raphael Barbo <rsbarbo@gmail.com>
2016-10-19 10:37:46 -0600

    update station

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,11 +2,16 @@ class Station
   attr_reader :service
 
   def initialize(params)
-    binding.pry
     @service ||= StationService.new(params, token: ENV['user_token'])
   end
 
   def find_all_stations
     service.get_all_stations
   end
+
+  def fuel_stations
+    binding.pry
+    find_all_stations
+  end
+
 end
